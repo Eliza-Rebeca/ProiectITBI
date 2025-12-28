dfa20763c280b8a4ca8c655a281721029d726e83
Boros Eliza-Georgiana <eliza.georgiana0920@gmail.com>
2025-12-28 22:51:55 +0200

    Am modificat modul de afisare al datelor astefl incat sa nu fie atat de mecanic deoarece pot surveni modificari la nivel de numar de caractere, dupa care am reusit sa iau toate informatiile din toate fisierele /var/log/auth.log [0-4], adica si cele arhivate. Am adaugat parametri pentru a verifica ce optiuni au fost introduse, in prezent este functionarea rularea fara optiuni si cea cu optiunea -n exclusiv.

--- script.sh
+++ script.sh
@@ -1,21 +1,48 @@
 #!/bin/bash
 
+n=0
+p=0
+s=0
+t=0
+
 verificare_optiune(){
 
 COMANDA=$1
 CONTENT=$2
+
         if [ "$COMANDA" == "-n" ]; then
-                tac /var/log/auth.log | grep -a "authentication failure" | grep -v "/var/log/auth.log" | head -n "$CONTENT" | while read -r linie; do
-                        echo -e "${linie:179:10}\t${linie:55:12}\t${linie:154:4}\t${linie:0:10} ${linie:11:8}"
-                done
+                zcat -f /var/log/auth.log.4.gz /var/log/auth.log.3.gz /var/log/auth.log.2.gz /var/log/auth.log.1 /var/log/auth.log | tac | grep -a "authentication failure" | grep -v "/var/log/auth.log" | head -n "$CONTENT" | sed -E 's/^([^ ]+).*gdm-password\]:.*tty=\/dev\/([^ ]+).*user=([^ ]+)/\3 gdm-password \2 \1/' | sed 's/T/ /; s/\..*+02:00//'
+                
         fi
 }
 
+ok=0
         while [ "$#" -gt 0 ]; do
 
-                if [[ "$1" == "-n" || "$1" == "-s" || "$1" == "-t" || "$1" == "-p" ]]; then
-                         verificare_optiune "$1" "$2"  #automat daca avem un option avem si un parametru dupa
-                         shift 2
+                if [ "$1" == "-n" ]; then
+			 n=1
+                         shift 1
+			 ok=1
+		elif [ "$1" == "-p" ]; then
+                         p=1
+                         shift 1
+                         ok=1
+                elif [ "$1" == "-s" ]; then
+                         s=1
+                         shift 1
+                         ok=1
+   		elif [ "$1" == "-t" ]; then
+                         t=1
+                         shift 1
+                         ok=1
                 fi
-        done
+
+	if [ "$n" -eq 1 ]; then
+		verificare_optiune "-n" "$1"
+		shift 1
+	fi
+done
+	if [ "$ok" -eq 0 ]; then
+	zcat -f /var/log/auth.log.4.gz /var/log/auth.log.3.gz /var/log/auth.log.2.gz /var/log/auth.log.1 /var/log/auth.log | tac | grep -a "authentication failure" | grep -v "/var/log/auth.log" | sed -E 's/^([^ ]+).*gdm-password\]:.*tty=\/dev\/([^ ]+).*user=([^ ]+)/\3 gdm-password \2 \1/' | sed 's/T/ /; s/\..*+02:00//'
+fi
 
